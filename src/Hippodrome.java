7704e63869f02be405ee321549a1376fff6bbd56
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-02-05 18:32:45 +0200

    MyFirstGame

---
+++ src/Hippodrome.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses;
+    static Hippodrome game;
+    public static void main(String[] args) throws InterruptedException {
+        game = new Hippodrome(new ArrayList<>());
+        Horse horse1 = new Horse("Molly", 3, 0);
+        Horse horse2 = new Horse("Billy", 3, 0);
+        Horse horse3 = new Horse("Willy", 3, 0);
+        game.getHorses().add(horse1);
+        game.getHorses().add(horse2);
+        game.getHorses().add(horse3);
+
+        game.run();
+
+    }
+
+    public Hippodrome(List<Horse> horses){
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public void run() throws InterruptedException {
+        for (int i = 1; i < 101; i++) {
+            move();
+            print();
+            Thread.sleep(3000);
+        }
+    }
+
+    public void move(){
+        for (Horse horse : horses) {
+            horse.move();
+        }
+    }
+
+    public void print(){
+        for (Horse horse : horses) {
+            horse.print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+}
+
